f95b772d5aa1813a2a72c99ab30be4e8dfcff45c
collins kiprotich <collinskiprotich@gmail.com>
2022-11-14 15:51:22 +0300

    add event listener on hamburger menu

--- script/main.js
+++ script/main.js
@@ -3,6 +3,14 @@ let menuDiv = document.createElement('div');
 menuDiv.className = 'menu-content';
 menuDiv.style.display = 'none';
 
-const divContent = '<img src="./icons/cross.png" alt=""> <a href="#portfolio">Portfolio</a> <a href="#aboutMe">About</a> <a href="#contactForm">Contact</a>';
+const divContent = '<img class="cross-icon" src="./icons/cross.png" alt=""> <a href="#portfolio">Portfolio</a> <a href="#aboutMe">About</a> <a href="#contactForm">Contact</a>';
 menuDiv.innerHTML = divContent;
 body.appendChild(menuDiv);
+let hamburgerIcon = document.querySelector(".hamburger-menu");
+hamburgerIcon.addEventListener("click",()=> {
+    menuDiv.style.display="flex";
+});
+let crossIcon = document.querySelector(".cross-icon");
+crossIcon.addEventListener("click", ()=> {
+    menuDiv.style.display="none";
+});
