a4ac2b7164f656c64474467910047e36f0e1ba90
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-15 14:10:14 +0530

    change js code for open popup window

--- script/main.js
+++ script/main.js
@@ -28,77 +28,78 @@ itemList.forEach((item) => {
 
 let projects = [
   {
-    id:"project-1",
+    id:"project1",
     h1:'Multi-Post Stories',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
     language: ['css','html','bootstrap', 'Ruby'],
     image: './images/Project-1.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-2",
+    id:"project2",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-3",
+    id:"project3",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-4",
+    id:"project4",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-5",
+    id:"project5",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-6",
+    id:"project6",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   },
   {
-    id:"project-7",
+    id:"project7",
     h1:'Profesional Art Printing Data',
     p:"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
     language: ['html','bootstrap', 'Ruby'],
     image: './images/Project-3.png',
-    btn1:'https://github.com/rbhatt1999/portfolio',
+    btn1:'https://rbhatt1999.github.io/Portfolio/',
     btn2:'https://github.com/rbhatt1999/portfolio'
   }  
 ];
+
+let projectArray =[];
 
 for (const item of projects) {
 let list = ``;
   for (const item1 of item.language) {
     list = list + `<li>${item1}</li>`
   }
-let projectPopupContent = `<div id ="${item.id}" class="popup-container">
-<div class="headline-language">
+let projectPopupContent = `<div class="headline-language">
   <div class="headline-cross">
     <h1>${item.h1}</h1>
     <img src="./icons/cross.png" alt="">
@@ -110,24 +111,28 @@ let projectPopupContent = `<div id ="${item.id}" class="popup-container">
 <div class="image-paragraph-button">
   <img src="${item.image}" alt="">
   <div class="paragraph-button">
-    ${item.p}
+    <p>${item.p}</p>
     <div class="buttons">
-      <a href="${item.btn1}"><button class="see-live">See live <img src="./icons/Icon-see live.svg" alt=""></button></a>
-      <a href="${item.btn2}"><button class="see-source">See live <img src="./icons/Icon -GitHub.png" alt=""></button></a>
+      <a href="${item.btn1}" target="_blank"><button class="see-live">See live <img src="./icons/Icon-see live.svg" alt=""></button></a>
+      <a href="${item.btn2}" target="_blank"><button class="see-source">See Source <img src="./icons/Icon -GitHub.png" alt=""></button></a>
     </div>
   </div>
-</div>
 </div>`;
-main.innerHTML+=projectPopupContent;
+projectArray.push(projectPopupContent);
+};
+
+for (let i = 0;i<projects.length;i++) {
+  projects[i].id =document.createElement('div');
+  projects[i].id.className='popup-container';
+  projects[i].id.innerHTML= projectArray[i];
+  body.appendChild(projects[i].id);
 };
 
 let projectButtons = document.querySelectorAll('.work-container button');
 let projectList= document.querySelectorAll('.popup-container');
-
 for (let i = 0; i<projectButtons.length;i++) {
-  console.log(i);
   projectButtons[i].addEventListener('click', () => {
-    console.log("button clicked");
-    projectList[0].style.display = 'flex';
+    projectList[i].style.display = 'flex';
+    main.style.filter='blur(10px)';
   });
 };
